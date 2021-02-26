b305b8fc06c89e168c627432829b4e7048bcbdd2
Jonathan Pinto Sperafico <jhonny.potter@gmail.com>
2021-02-26 21:38:57 +0100

    satisfatory implementation of a web component.

--- components-v2/abstract.js
+++ components-v2/abstract.js
@@ -1,13 +1,41 @@
 class AbstractClass extends HTMLElement {
-    _parser = new DOMParser();
-    _separeteHtml() {
-        let name = this._correctName;
-        let html = fetch(`./components-v2/${name}/${name}.html`)
-            .then(response => response.text())
-            .catch(error => { throw error; });
-        return html;
+
+    constructor() {
+        super();
+        if (this.constructor == AbstractClass) {
+            throw new Error(`Abstract classes can't be initialized.`);
+        }
+       
+        this._template = fetch(`${this.#path}.html`)
+                .catch(error => { throw error; });
+        this._listeners = [];
     }
-    get _correctName() {
+
+    get template() {
+        return this._template.then(response => response.text())
+            .then(html => {
+                let name = this.#tag;
+                let doc = new DOMParser().parseFromString(html, 'text/html');
+                let template = doc.getElementById(name);
+                let css = doc.querySelector('link');
+                css.href = `${this.#path}.css`;
+                
+                return [template, css];
+            });
+    }
+
+    get #tag() {
         return this.constructor.name.match(/[A-Z][a-z0-9]+|[0-9][a-z0-9]+/g).join('-').toLocaleLowerCase();
+    }
+
+    get #path() {
+        let name = this.#tag;
+        let value = "";
+        if (window.location.href.includes(name)) {
+            value = name;
+        } else {
+            value = `./components-v2/${name}/${name}`;
+        }
+        return value;
     }
 }
--- components-v2/menu-item/menu-item.js
+++ components-v2/menu-item/menu-item.js
@@ -10,25 +10,47 @@ class MenuItem extends AbstractClass {
     //      - define an array of attributes to be watched.
     constructor() {
         super();
-
-        this._separeteHtml().then(html => {
-            let doc = this._parser.parseFromString(html, 'text/html');
-            let template = doc.getElementById(this._correctName);
-            let shadow = this.attachShadow({mode: 'open'});
-            shadow.innerHTML = template.outerHTML;
-        });
+        // initialize aditional data
     }
 
     connectedCallback() {
+        this.template.then(([template, css]) => {
+            let shadow = this.attachShadow({mode : 'open'});
 
+            shadow.appendChild(css.cloneNode(true));
+            shadow.appendChild(template.content.cloneNode(true));
+        });
+
+        // remember to set any listeners over here.
+    }
+
+    static get observedAttributes() {
+        // must remember to create get/set property to bind attr with rendered html.
+        return ['position'];
+    }
+
+    attributeChangedCallback(attr, oldValue, newValue) {
+        if (oldValue !== newValue) {
+            this[attr] = newValue;
+        }
+    }
+
+    get position() {
+        // This must work.
+        console.log(`Position changed to: ${this.getAttribute('position')}`);
+
+        // must never do this, because will trigger endless loop.
+        // console.log(`Position changed to: ${this['position']}`);
+    }
+
+    set position(value) {
+        console.log(`Position will change to: ${value}`);
     }
 
     disconnectedCallback() {
-
+        //if listeners were added, remember to remove them.
     }
 }
 
+//Migrate to a smart component factory.
 customElements.define('menu-item', MenuItem);
-
-// using as refference:
-// - https://stackoverflow.com/questions/55080103/how-to-separate-web-components-to-individual-files-and-load-them
